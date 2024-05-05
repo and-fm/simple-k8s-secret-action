7f03a1a0133f035fa07b0ad44cf3c979168d7d27
npgy <me@np.gy>
2024-05-05 19:33:01 -0400

    try auto semver from changelog

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,8 @@
 #!/bin/sh -l
 
 # run some kubectl commands
+
+kubectl create secret generic gocamp-secrets --dry-run=client \
+            --from-literal=JWT_SECRET=random_secret \
+            --from-literal=USERNAME=nick \
+            -n gocamp-dev -o yaml >> $GITHUB_OUTPUT
