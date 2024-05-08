5c935991a56b3067b9291e6accd76cc5cb244789
npgy <me@np.gy>
2024-05-08 11:36:01 -0400

    try this

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,9 @@
 
 echo "$3" >> secrets.txt
 
-echo "secret=$(secretparse $1 $2)" >> $GITHUB_OUTPUT
+echo 'secret<<EOF' >> $GITHUB_OUTPUT
+secretparse $1 $2 >> $GITHUB_OUTPUT
+echo "EOF" >> $GITHUB_OUTPUT
 
 # kubectl create secret generic gocamp-secrets --dry-run=client \
 #             --from-literal=JWT_SECRET=random_secret \
