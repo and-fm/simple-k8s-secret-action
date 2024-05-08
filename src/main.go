ddeb077c352967b610429e8763331c815719d5fe
npgy <me@np.gy>
2024-05-08 08:05:58 -0400

    create yaml from secrets file

--- src/main.go
+++ src/main.go
@@ -18,35 +18,32 @@ func main() {
 		panic("must provide the secret name and namespace args!")
 	}
 
-	fmt.Println(secretName)
-
 	secretsFile, err := os.Open("secrets.txt")
-	secretCmdArgs := make([]string, 7);
 
 	if err != nil {
 		panic("error reading secrets!")
 	}
 
-	secretCmdArgs[0] = "kubectl";
-	secretCmdArgs[1] = fmt.Sprintf("create secret generic %s --dry-run=client \\", secretName);
+	initArgs := []string{"create", "secret", "generic", secretName, "--dry-run=client"}
+
+	createSecretCmd := exec.Command("kubectl", initArgs...);
 
 	fileScanner := bufio.NewScanner(secretsFile)
 	fileScanner.Split(bufio.ScanLines)
 
-	// var fileLines []string
-	i := 2
 	for fileScanner.Scan() {
 		text := fileScanner.Text()
 		parts := strings.Split(text, ":")
-		secretCmdArgs[i] = fmt.Sprintf("--from-literal=%s=%s \\", parts[0], parts[1]);
-		fmt.Printf("secret name: %s, value: %s\n", parts[0], parts[1])
-		i++
+		createSecretCmd.Args = append(createSecretCmd.Args, fmt.Sprintf("--from-literal=%s=%s", parts[0], parts[1]));
 	}
 
-	secretCmdArgs = append(secretCmdArgs, fmt.Sprintf("-n %s -o yaml", secretNamespace))
+	finalArgs := []string{"-n", secretNamespace, "-o", "yaml"}
 
-	createSecretCmd := exec.Command(secretCmdArgs[0], secretCmdArgs[1:]...);
-	
-	// out, err := strings.Join(createSecretCmd.Args, " ")
-	fmt.Print(strings.Join(createSecretCmd.Args, " "))
+	createSecretCmd.Args = append(createSecretCmd.Args, finalArgs...)
+
+	bytes, err := createSecretCmd.CombinedOutput()
+	if err != nil {
+		panic(fmt.Sprintf("error executing kubectl: %s", err))
+	}
+	fmt.Print(string(bytes[:]))
 }
