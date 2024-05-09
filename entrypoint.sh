d93688e199201fe46cb6965d41b8be2ddd33c3bd
npgy <me@np.gy>
2024-05-09 19:55:32 -0400

    new spice

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,20 +1,49 @@
 #!/bin/sh -l
 
-echo "$3" >> secrets.txt
-
 echo "kubectl create secret generic $1 --dry-run=client \\" > kubecmd.sh
 
-while IFS="" read -r s || [ -n "$s" ]
-do
-    name=$(echo $s | cut -d ':' -f 1)
-    value=$(echo $s | cut -d ':' -f 2-)
-    echo "--from-literal=$name=$value \\" >> kubecmd.sh
-done < secrets.txt
+# if generic secret
+if [ -n $3 ]
+then
+    echo "$3" >> secrets.txt
 
-echo "-n $2 -o yaml" >> kubecmd.sh
+    while IFS="" read -r s || [ -n "$s" ]
+    do
+        name=$(echo $s | cut -d ':' -f 1)
+        value=$(echo $s | cut -d ':' -f 2-)
+        echo "--from-literal=$name=$value \\" >> kubecmd.sh
+    done < secrets.txt
 
-chmod +x kubecmd.sh
+    echo "-n $2 -o yaml" >> kubecmd.sh
 
-echo 'secret_yaml<<EOF' >> $GITHUB_OUTPUT
-./kubecmd.sh >> $GITHUB_OUTPUT
-echo "EOF" >> $GITHUB_OUTPUT
+    chmod +x kubecmd.sh
+
+    echo 'secret_yaml<<EOF' >> $GITHUB_OUTPUT
+    ./kubecmd.sh >> $GITHUB_OUTPUT
+    echo "EOF" >> $GITHUB_OUTPUT
+
+# if basic auth
+elif [ -n $4 ]
+then
+    username=$(echo $4 | cut -d ':' -f 1)
+    password=$(echo $4 | cut -d ':' -f 2-)
+    echo "--from-literal=username=$username \\" >> kubecmd.sh
+    echo "--from-literal=password=$password \\" >> kubecmd.sh
+
+    echo "--type kubernetes.io/basic-auth -n $2 -o yaml" >> kubecmd.sh
+
+    chmod +x kubecmd.sh
+
+    echo 'secret_yaml<<EOF' >> $GITHUB_OUTPUT
+    ./kubecmd.sh >> $GITHUB_OUTPUT
+    echo "EOF" >> $GITHUB_OUTPUT
+    return 0
+fi
+
+# if configmap env
+elif [ -n $5 ]
+then
+    echo "todo"
+fi
+
+fi
