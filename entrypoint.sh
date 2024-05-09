30cb5d13259cfb158671c9e101f99bb221ca9a25
npgy <me@np.gy>
2024-05-09 11:44:34 -0400

    ahh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,29 +1,26 @@
 #!/bin/sh -l
 
-# echo "$SECRETS" >> secrets.txt
-
-echo secret="$1"
-echo secret_ns="$2"
-echo secret1="$3"
-echo secret2="$4"
+echo "$3" >> secrets.txt
 
 echo "kubectl create secret generic $1 --dry-run=client \\" > kubecmd.sh
 
-# while IFS="" read -r s || [ -n "$s" ]
-# do
-#   name=$(cut -d ':' -f 1 <<< $s)
-#   value=$(cut -d ':' -f 2- <<< $s)
-#   echo "--from-literal=$name=$value \\" >> kubecmd.sh
-# done < secrets.txt
-
-for s in "${@:3}"
+while IFS="" read -r s || [ -n "$s" ]
 do
-    name=$(echo $s | cut -d ':' -f 1)
-    value=$(echo $s | cut -d ':' -f 2-)
-    echo "--from-literal=$name=$value \\" >> kubecmd.sh
-done
+  name=$(cut -d ':' -f 1 <<< $s)
+  value=$(cut -d ':' -f 2- <<< $s)
+  echo "--from-literal=$name=$value \\" >> kubecmd.sh
+done < secrets.txt
+
+# for s in "${@:3}"
+# do
+#     name=$(echo $s | cut -d ':' -f 1)
+#     value=$(echo $s | cut -d ':' -f 2-)
+#     echo "--from-literal=$name=$value \\" >> kubecmd.sh
+# done
 
 echo "-n $2 -o yaml" >> kubecmd.sh
+
+echo "$(cat kubecmd.sh)"
 
 chmod +x kubecmd.sh
 
