7e802dc4d65fe4adb483b73cc227f8ef988c878c
npgy <me@np.gy>
2024-05-09 11:52:03 -0400

    switch to pure bash

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,13 +11,6 @@ do
     echo "--from-literal=$name=$value \\" >> kubecmd.sh
 done < secrets.txt
 
-# for s in "${@:3}"
-# do
-#     name=$(echo $s | cut -d ':' -f 1)
-#     value=$(echo $s | cut -d ':' -f 2-)
-#     echo "--from-literal=$name=$value \\" >> kubecmd.sh
-# done
-
 echo "-n $2 -o yaml" >> kubecmd.sh
 
 chmod +x kubecmd.sh
