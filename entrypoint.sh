1ca23ef988e948b8a543c506bd4e109a92ec5649
npgy <me@np.gy>
2024-05-09 20:45:50 -0400

    now does it break or what

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,30 +14,24 @@ if [ ! -z "$3" ]; then
     done < data.txt
 
     echo "-n $2 -o yaml" >> kubecmd.sh
+
+# if basic auth
+elif [ ! -z "$4" ]; then
+    username=$(echo $4 | cut -d ':' -f 1)
+    password=$(echo $4 | cut -d ':' -f 2-)
+
+    echo "--from-literal=username="$username" \\" >> kubecmd.sh
+    echo "--from-literal=password="$password" \\" >> kubecmd.sh
+
+    echo "--type kubernetes.io/basic-auth -n $2 -o yaml" >> kubecmd.sh
+
+# if configmap env
+elif [ ! -z "$5" ]; then
+    echo "todo"
+
+else
+    echo "Please use one of secrets, basic_auth, or configmap_env"
 fi
-# # if basic auth
-# elif [ ! -z "$4" ]; then
-#     username=$(echo $4 | cut -d ':' -f 1)
-#     password=$(echo $4 | cut -d ':' -f 2-)
-
-#     echo "--from-literal=username="$username" \\" >> kubecmd.sh
-#     echo "--from-literal=password="$password" \\" >> kubecmd.sh
-
-#     echo "--type kubernetes.io/basic-auth -n $2 -o yaml" >> kubecmd.sh
-
-# # if configmap env
-# elif [ ! -z "$5" ]; then
-#     echo "todo"
-
-# else
-#     echo "Please use one of secrets, basic_auth, or configmap_env"
-# fi
-
-# chmod +x kubecmd.sh
-
-# echo 'out_yaml<<EOF' >> $GITHUB_OUTPUT
-# ./kubecmd.sh >> $GITHUB_OUTPUT
-# echo "EOF" >> $GITHUB_OUTPUT
 
 chmod +x kubecmd.sh
 
